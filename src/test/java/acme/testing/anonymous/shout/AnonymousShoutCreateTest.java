82cd5f8792c1a10e6ffcc73eeba42f77e2918cd6
Daniel J. Martínez Suárez <djmartinezsuarez91@gmail.com>
2021-05-17 20:32:57 +0200

    Add anonymous positive tests (Level C)

---
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
@@ -0,0 +1,34 @@
+package acme.testing.anonymous.shout;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AnonymousShoutCreateTest extends AcmePlannerTest {
+	
+	// Lifeycle management ------------------------------
+	
+	// Test cases ---------------------------------------
+	
+	@DisplayName("Write shouts and list them")
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/shout/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void createPositive(final int recordIndex, final String author, final String text, final String info) {		
+		super.clickOnMenu("Anonymous", "New shout");
+
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.fillInputBoxIn("info", info);
+		super.clickOnSubmitButton("Shout!");
+		
+		super.clickOnMenu("Anonymous", "List shouts");
+
+		super.checkColumnHasValue(0, 1, author);
+		super.checkColumnHasValue(0, 2, text);
+	}
+
+}
